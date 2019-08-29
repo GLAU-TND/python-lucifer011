447bc80c693fb48b761faae6b0cfe000e3d1a677
lucifer011 <anushka.soni.6667@gmail.com>
2019-08-29 17:43:05 +0530

    Updated

---
+++ Bottle Necks Solution.py
@@ -0,0 +1,12 @@
+#
+#@author AnushkaSoni
+#@username lucifer011
+#@version 1.0
+#
+n = int(input("Enter number of bottles"))
+lst = list(map(int,input().split(" ")))
+fill=[]
+for i in range(0,n):
+    fill.append(lst.count(i))
+
+print(max(fill))
